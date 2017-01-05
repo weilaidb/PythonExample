0bcc01b2efc230bd7de5636f212a6fbf0368cf05
weilaidb <weilaidb@localhost.localdomain>
2017-01-05 08:40:30 -0800

    add linux-3.0.8 to one file

---
+++ catresult.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -z $1 ];then
+	echo "./usage.sh abspath"
+	exit
+	
+fi
+CURPATH=`pwd`
+echo "current path:$CURPATH"
+echo "to path     :$CURPATH/$1"
+cd $CURPATH/$1
+find . -type f  > filelist
+savefile=cat.result.txt
+rm -rf $savefile
+i=0
+totalnum=`cat filelist | wc -l`
+while read line
+do
+	((i++))
+	((per=$i*100/$totalnum))
+	# echo "no: $((i)), totalnum: $totalnum, %$((per))"
+	echo "%$((per))"
+	cat $line >> $savefile
+done < filelist
+
+echo "all over!!"
+echo "all done!!"
